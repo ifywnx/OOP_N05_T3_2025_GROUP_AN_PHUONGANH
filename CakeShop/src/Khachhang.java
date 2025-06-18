690f4c9f5464e3689a5ab49dcb28869eb70309d4
ifywnx <annguyen.ub0@gmail.com>
2025-06-18 08:33:02 +0000

    update khachhang

--- CakeShop/src/Khachhang.java
+++ CakeShop/src/Khachhang.java
@@ -8,9 +8,9 @@ public class Khachhang {
 
     // phuong thuc hoat dong - methods
     //shift-alt-F: định dang dữ liẹu 
-
-    public String setMaKhachHang(String name){
-        MaKhachHang = name;
+CakeShop/src
+    public String setMaKhachHang(String ma){
+        MaKhachHang = ma;
 
         return MaKhachHang;
     }
@@ -23,5 +23,33 @@ public class Khachhang {
 
    public Khachhang(){
 
+    }
+    public class String setTenKhachHang(String name){
+        TenKhachHang = name;
+
+        return TenKhachHang;
+    }
+
+    public void getTenKhachHang(){
+        System.out.println("Hien thi ten khach hang" + TenKhachHang);
+    }
+
+    private class String setDiaChi(String diachi){
+        DiaChi = diachi;
+
+        return DiaChi;
+    }
+    private void getDiaChi(){
+        System.out.println("An dia chi" + DiaChi);
+    }
+
+    private class String setSoDienThoai(String sdt){
+        SoDienThoai = sdt;
+
+        return sdt;
+    }
+
+    private void getSoDienThoai(){
+        System.out.println("An so dien thoai" + SoDienThoai);
     }
 }
---
+++ CakeShop/src/Sanpham.java
@@ -0,0 +1,10 @@
+public class Sanpham {
+
+    // Biến miêu tả - Member variable
+   String MaSanPham
+   String TenSanPham
+   int SoLuong
+   String NhaSX
+   int HanSudung
+   String NguyenLieu
+   
