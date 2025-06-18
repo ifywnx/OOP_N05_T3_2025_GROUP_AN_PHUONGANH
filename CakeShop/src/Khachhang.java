9a8ff56c8ca6512c72feb14b07ffc2d5b0311c11
Le Thu Nguyen <nglthu1967@gmail.com>
2025-06-18 07:45:38 +0000

    Class KhachHang

---
+++ CakeShop/src/Khachhang.java
@@ -0,0 +1,22 @@
+public class Khachhang {
+
+    // Biến miêu tả - Member variable
+    String MaKhachHang;
+    String TenKhachHang;
+    String DiaChi;
+    String SoDienThoai;
+
+    // phuong thuc hoat dong - methods
+    //shift-alt-F: định dang dữ liẹu 
+
+    public String setMaKhachHang(String name){
+        MaKhachHang = name;
+
+        return MaKhachHang;
+    }
+
+    public void getMaKhachHang(){
+        System.out.println("Hien thi ma khach hang" + MaKhachHang);
+    }
+
+}
