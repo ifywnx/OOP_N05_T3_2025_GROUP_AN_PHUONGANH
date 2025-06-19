b7aae1c715151cc5f928967feba3e0e278ccd8a7
ifywnx <annguyen.ub0@gmail.com>
2025-06-19 18:38:32 +0700

    Update Giaodich.java

--- CakeShop/src/Giaodich.java
+++ CakeShop/src/Giaodich.java
@@ -1,10 +1,23 @@
 public class Giaodich {
 
     //bien mieu ta cho giao dich
+    Private String MaGiaoDich;
+    String NgayThangNamGiaoDich;
+    String GioGiaoDich;
+    double TongTien;
 
 
     //phuong thuc hoat dong
-
+public String setMaGiaoDich(String mgd){
+    MaGiaoDich = mgd;
+    return MaGiaoDich;
+}
+public void getMaGiaoDich(){
+    System.out.println("Ma giao dich" + MaGiaoDich);
+}
     
+public String setNgayThangNamGiaoDich(String ntngd){
+    NgayThangNamGiaoDich = ntngd;
+}
     
 }
