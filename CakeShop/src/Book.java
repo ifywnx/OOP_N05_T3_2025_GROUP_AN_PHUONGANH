2627356400947af534d8b0b317730918707aa1d0
ifywnx <annguyen.ub0@gmail.com>
2025-07-15 09:26:17 +0000

    ud

--- CakeShop/src/Book.java
+++ CakeShop/src/Book.java
@@ -1,11 +1,11 @@
 public class Book {
- String title;
- String author;
- int numPages;
- Book() { } ; // default constructor
- public Book(String t, String a, int p) {
- title = t;
- author = a;
- numPages = p;
- }
+    private String title;
+
+    public Book(String t) {
+        title = t;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
