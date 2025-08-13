924355d9d9517d5f9a8bcfec1f5b1cf91a1f7180
anhlephuong <phuonganhhd6789@gmail.com>
2025-08-13 09:26:55 +0000

    fix NotFoundException

--- complete/src/main/java/com/example/servingwebcontent/exception/NotFoundException.java
+++ complete/src/main/java/com/example/servingwebcontent/exception/NotFoundException.java
@@ -1,5 +1,67 @@
 package com.example.servingwebcontent.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) { super(message); }
+    
+    public NotFoundException(String message) {
+        super(message);
+    }
+    
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
+// BusinessException.java
+package com.example.servingwebcontent.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BusinessException extends RuntimeException {
+    
+    public BusinessException(String message) {
+        super(message);
+    }
+    
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
+// ValidationException.java
+package com.example.servingwebcontent.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ValidationException extends RuntimeException {
+    
+    public ValidationException(String message) {
+        super(message);
+    }
+    
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
+// InsufficientStockException.java
+package com.example.servingwebcontent.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class InsufficientStockException extends BusinessException {
+    
+    public InsufficientStockException(String productName, Integer requestedQuantity, Integer availableQuantity) {
+        super(String.format("Không đủ hàng trong kho! Sản phẩm '%s': Yêu cầu %d, còn lại %d", 
+                           productName, requestedQuantity, availableQuantity));
+    }
 }
